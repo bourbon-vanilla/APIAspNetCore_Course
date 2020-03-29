c8b58758dc23f436ab44b5621cb2ac2c92c0a55f
Raphael <bourbon-vanilla@outlook.com>
2020-03-29 23:03:22 +0200

    Finisch of 6. EF Core introduction: 1. Adding migrations; 2. Siding the database with sample data via migration;

---
+++ Starter files/CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    internal class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You should provide the name value")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        // It would also work in this case by a convension without setting the [ForeignKey] attribute, 
+        // but if we change the name of the property it would not work anymore
+        [ForeignKey(nameof(CityId))] 
+        public City City { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
--- Starter files/CityInfo.API/Program.cs
+++ Starter files/CityInfo.API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Contexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
@@ -20,7 +23,22 @@ namespace CityInfo.API
             try
             {
                 logger.Info("Initializing applicatiion...");
-                CreateHostBuilder(args).Build().Run();
+                var host = CreateHostBuilder(args).Build();
+
+                using(var scope = host.Services.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetService<CityInfoContext>();
+
+                    // if for demo purposes - delete existing & create fresh database on sql-server
+                    dbContext.Database.EnsureDeleted();
+                    // You
+                    var createdNow = dbContext.Database.EnsureCreated();
+                    // or if you dont delete you migrate the existing database, if the db is not the latest
+                    if (!createdNow)
+                        dbContext.Database.Migrate(); // The migration will be done only if not latest db schema
+                }
+                    
+                host.Run();
                 logger.Info("Initializing application done.");
             }
             catch (Exception ex)
